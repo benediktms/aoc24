7829c2ae178faff9f32bd1891ae2db5644747bff
Benedikt Schnatterbeck <benedikt.schnatterbeck@fresha.com>
2024-12-04 22:05:49 +0000

    feat: solve day1 part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,19 +1,45 @@
+use core::num;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::path::Path;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let f = File::open("input.txt")?;
-    let mut reader = BufReader::new(f);
-    let lines = reader.lines().map(|input| {
-        if let Ok(line) = input {
-            line.split("   ")
-                .map(|nums| nums.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>()
-        } else {
-            vec![0, 0]
-        }
-    });
+    let file = File::open(Path::new("./day1/src/input.txt"))?;
+    let reader = BufReader::new(file);
+
+    let mut lhs: Vec<i32> = Vec::new();
+    let mut rhs: Vec<i32> = Vec::new();
+
+    let pairs = reader
+        .lines()
+        .map(|input| {
+            if let Ok(line) = input {
+                let numbers = line
+                    .split("   ")
+                    .map(|n| n.parse::<i32>().expect("Failed to parse number"))
+                    .collect::<Vec<i32>>();
+
+                lhs.push(numbers[0]);
+                rhs.push(numbers[1]);
+
+                numbers
+            } else {
+                vec![0, 0]
+            }
+        })
+        .collect::<Vec<Vec<i32>>>();
+
+    lhs.sort();
+    rhs.sort();
+
+    let sum = lhs
+        .iter()
+        .zip(rhs.iter())
+        .map(|(a, b)| (a - b).abs())
+        .sum::<i32>();
+
+    println!("{:?}", sum);
 
     Ok(())
 }
